dd4a5b84ebc2ed44282aad780ce512424049dfb9
Kelly <kellytso04@gmail.com>
2021-10-27 19:27:28 -0400

    Created funct component StyleThumbnail

---
+++ client/src/components/overview/styleThumbnail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const StyleThumbnail = (props) => {
+  return (
+    <image className='style-thumbnail'>I am a style thumbnail</image>
+  );
+}
+
+export default StyleThumbnail;
