0f0df0f8052999e63aab7cab142956f99ca20af9
Arun Bhardwaj <arun.b.95@gmail.com>
2021-10-28 18:39:45 -0400

    added search icon

--- client/src/components/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-
 const QuestionsAnswers = () => {
 
   return (
-    <div>Q {"&"} A</div>
+    <div>
+      Q {"&"} A
+      <form>
+        <label htmlFor="search">Search</label>
+        <input id="search"></input>
+        {/* THIS IS THE SEARCH ICON, SHOULD PROBABLY IMPORT SOMEHOW? */}
+        <img src={"./images/search.svg"} alt="search bar" height="15" width="15"/>
+      </form>
+    </div>
   )
 }
 
