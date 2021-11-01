5fa618475661cb102b83055fbc536613813eb67e
Arun Bhardwaj <arun.b.95@gmail.com>
2021-11-01 12:58:13 -0400

    adds jsdoc package to generate jsdocumentation

--- client/src/request.js
+++ client/src/request.js
@@ -5,7 +5,7 @@ import {TOKEN} from '../../config.js';
  * Returns a promise that resolves to all questions for a given product.
  *
  * @param {number} productId The product_id you want questions for.
- * @returns {Promise} Promise object representing api results
+ * @returns {Promise<any>} Promise object representing api results
  */
 export function getAllQuestions(productId) {
   return axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/qa/questions', {
