20e5ab547bc55c35c0d0bafca6e71dd6a1ead96c
Arun Bhardwaj <arun.b.95@gmail.com>
2021-11-01 12:41:39 -0400

    adds api request helper file. This should store all our necessary api calls

--- client/src/components/QA/QASection.jsx
+++ client/src/components/QA/QASection.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Search from './Search.jsx';
 import QuestionsList from './QuestionsList.jsx';
+import { getAllQuestions } from '../../request.js';
 
 const QASection = () => {
+  getAllQuestions(39333)
+    .then(results => console.log(results.data));
+
   return (
     <div>
-      {/* we wnat it to have font color #525252 */ }
+      {/* we want it to have font color #525252 */ }
       QUESTIONS {"&"} ANSWERS
       <Search />
       <QuestionsList />
--- client/src/components/QA/Search.jsx
+++ client/src/components/QA/Search.jsx
@@ -8,7 +8,7 @@ const Search = () => (
       <StyledLabel htmlFor="search">Search</StyledLabel>
       <SearchContainer>
         <StyledInput id="search" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."></StyledInput>
-        {/* THIS IS THE SEARCH ICON, SHOULD PROBABLY IMPORT SOMEHOW? */}
+        {/* TODO: THIS IS THE SEARCH ICON, WE SHOULD PROBABLY IMPORT instead of using src directly? */}
         <img src={"./images/search.svg"} alt="search bar" height="15" width="15"/>
       </SearchContainer>
     </form>
