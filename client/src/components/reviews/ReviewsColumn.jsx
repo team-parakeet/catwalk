bfe1f3a8d3e080eda8af4afe40f7fb360649eced
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-01 22:14:25 -0400

    converted to functional component and passed down props

--- client/src/components/reviews/ReviewsColumn.jsx
+++ client/src/components/reviews/ReviewsColumn.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import RatingScore from "./RatingScore.jsx";
 import StarsChart from "./StarsChart.jsx"
 import ComparisonScales from "./ComparisonScales.jsx";
 
-class ReviewsColumn extends React.Component {
-  constructor(props) {
-    super(props)
+function ReviewsColumn({ avgRating, reviews }) {
     // what props does this component need?
     // what state does this component need?
-  }
 
-  render() {
-    return (
-      <div className="reviews-column-container">
-        <RatingScore />
-        <StarsChart />
-        <ComparisonScales/>
-      </div>
-    )
-  }
+  return (
+    <div className="reviews-column-container">
+      <RatingScore avgRating={avgRating}/>
+      <StarsChart reviews={reviews}/>
+      <ComparisonScales/>
+    </div>
+  )
 }
 
 export default ReviewsColumn;
