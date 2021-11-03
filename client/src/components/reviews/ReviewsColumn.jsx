29fc79639aca4ef9d9935f601c53eab2600428bf
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 14:20:54 -0400

    moved ratings state outside starsChart component so it can rather be passed down as props' '

--- client/src/components/reviews/ReviewsColumn.jsx
+++ client/src/components/reviews/ReviewsColumn.jsx
@@ -7,10 +7,22 @@ function ReviewsColumn({ reviews }) {
   // what props does this component need?
   // what state does this component need?
 
+  const [ratings, setRatings] = useState({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
+
+  useEffect(() => {
+    reviews.forEach(review => {
+      const currentRating = review.rating;
+      setRatings(prevState => ({
+        ...prevState,
+        [currentRating]: prevState[currentRating] + 1
+      }))
+    })
+  }, [reviews])
+
   return (
     <div className="reviews-column-container">
       <RatingScore reviews={reviews}/>
-      <StarsChart reviews={reviews}/>
+      <StarsChart ratings={ratings}/>
       <ComparisonScales/>
     </div>
   )
