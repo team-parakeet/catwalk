0e8384e47f831c04627210ca817fa64455e6aa2e
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 14:22:51 -0400

    removed state and useEffect logic to be received as props

--- client/src/components/reviews/StarsChart.jsx
+++ client/src/components/reviews/StarsChart.jsx
@@ -1,22 +1,10 @@
 import React, {useState, useEffect} from 'react';
 
-function StarsChart({ reviews }) {
+function StarsChart({ ratings }) {
   //once we get stars
   // get the keys
 
   const stars = [1, 2, 3, 4, 5];
-
-  const [ratings, setRatings] = useState({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
-
-  useEffect(() => {
-    reviews.forEach(review => {
-      const currentRating = review.rating;
-      setRatings(prevState => ({
-        ...prevState,
-        [currentRating]: prevState[currentRating] + 1
-      }))
-    })
-  }, [reviews])
 
   return (
     <div className="stars-chart">
