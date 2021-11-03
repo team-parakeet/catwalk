8384b6513b6f3cbbd4b136942d0b4a36977a6881
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 14:31:23 -0400

    added logic for the sortby dropdown

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { TOKEN } from '../../../../config.js';
 import ReviewsColumn from './ReviewsColumn.jsx';
-import SortBy from './SortBy.jsx';
 import ReviewsList from './ReviewsList.jsx';
 
 function Reviews({ reviews }) {
   // what props does this component need?
   // what state does this component need?
+
+  const handleOnChange = e => {
+    //take the value and send a request to fetch data by relevance
+    const selected = e.target.value;
+    const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/reviews/?sort=${selected}&product_id=39333`
+    // get request
+    axios.get(url, {
+      headers: {
+        Authorization: TOKEN
+      }
+    })
+    .then(r => console.log(r.data.results))
+  }
 
   return (
     <div className="reviews-container">
@@ -16,7 +30,14 @@ function Reviews({ reviews }) {
         <ReviewsColumn reviews={reviews} />
       </div>
       <div>
-        <SortBy />
+        <div className="sort=by=bar">
+          Sorted by:
+          <select onChange={handleOnChange}>
+            <option value="relevance">relevance</option>
+            <option value="helpful">helpful</option>
+            <option value="newest">newest</option>
+          </select>
+        </div>
         <ReviewsList reviews={reviews}/>
       </div>
       <div className="reviews-buttons">
