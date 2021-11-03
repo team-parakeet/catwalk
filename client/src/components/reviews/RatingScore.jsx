36e12f91bef98d2f23d87ea94a3740754a87e93b
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 12:47:06 -0400

    implemented avgRating logic to pass down as props

--- client/src/components/reviews/RatingScore.jsx
+++ client/src/components/reviews/RatingScore.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Score from './Score.jsx';
 import Stars from './Stars.jsx';
 
-function RatingScore(props) {
+function RatingScore({ reviews }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (reviews.length !== 0) {
+      setIsLoading(false);
+    }
+  }, [reviews]);
+
+  if (isLoading) {
+    return <div>IS LOADING..</div>
+  }
+
+  const getAvgRating = () => {
+    let sum = 0;
+    for (let i = 0; i < reviews.length; i++) {
+      let currentReview = reviews[i];
+      sum += currentReview.rating;
+    }
+    let result = sum / reviews.length;
+    return result;
+  }
+
+  const avgRating = getAvgRating();
+
   return (
     <div className="rating-score-container">
-      <Score avgRating={props.avgRating}/>
-      <Stars rating={props.avgRating}/>
+      <Score avgRating={avgRating}/>
+      <Stars rating={avgRating}/>
     </div>
   )
 }
