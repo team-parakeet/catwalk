bfbeedff3af52b013d18f518b5ebd29ebea00480
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 12:44:49 -0400

    moved avgRating logic down closer to where it is used and passed reviews as props instead

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -6,37 +6,13 @@ function Reviews({ reviews }) {
   // what props does this component need?
   // what state does this component need?
 
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (reviews.length !== 0) {
-      setIsLoading(false);
-    }
-  }, [reviews]);
-
-  const getAvgRating = () => {
-    let sum = 0;
-    for (let i = 0; i < reviews.length; i++) {
-      let currentReview = reviews[i];
-      sum += currentReview.rating;
-    }
-    let result = sum / reviews.length;
-    return result;
-  }
-
-  if (isLoading) {
-    return <div>IS LOADING..</div>
-  }
-
-  const avgRating = getAvgRating();
-
   return (
     <div className="reviews-container">
       <div className="reviews-header">
         RATINGS & REVIEWS
       </div>
       <div>
-        <ReviewsColumn avgRating={avgRating} reviews={reviews}/>
+        <ReviewsColumn reviews={reviews} />
         <ReviewsList reviews={reviews}/>
       </div>
       <div className="reviews-buttons">
