ea3304c687f218a714175a87d320a2d7421ab5ca
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 12:45:40 -0400

    moved avgRating logic down closer to where it is used and passed reviews as props instead

--- client/src/components/reviews/ReviewsColumn.jsx
+++ client/src/components/reviews/ReviewsColumn.jsx
@@ -3,26 +3,13 @@ import RatingScore from "./RatingScore.jsx";
 import StarsChart from "./StarsChart.jsx"
 import ComparisonScales from "./ComparisonScales.jsx";
 
-function ReviewsColumn({ avgRating, reviews }) {
+function ReviewsColumn({ reviews }) {
   // what props does this component need?
   // what state does this component need?
 
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    if (reviews.length !== 0) {
-      setIsLoading(false)
-    }
-  }, [reviews])
-
-  if (isLoading) {
-    return <div>IS LOADING..</div>
-  }
-
-
   return (
     <div className="reviews-column-container">
-      <RatingScore avgRating={avgRating}/>
+      <RatingScore reviews={reviews}/>
       <StarsChart reviews={reviews}/>
       <ComparisonScales/>
     </div>
