629faf240cbc39b98b7ea2daf623ce0f34ade2ed
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 18:45:45 -0400

    added ReviewsContainer component

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,60 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { TOKEN } from '../../../../config.js';
 import ReviewsColumn from './ReviewsColumn.jsx';
-import ReviewsList from './ReviewsList.jsx';
+import ReviewsContainer from './ReviewsContainer.jsx';
 
 function Reviews({ reviews }) {
   // what props does this component need?
   // what state does this component need?
-  const [currentReviews, setCurrentReviews] = useState(null);
-
-  useEffect(() => {
-    if (reviews.length !== 0) {
-      setCurrentReviews(reviews)
-    }
-  }, [reviews])
-
-  if (!currentReviews) {
-    return null
-  }
-
-  const handleOnChange = e => {
-    //take the value and send a request to fetch data by relevance
-    const selected = e.target.value;
-    const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/reviews/?sort=${selected}&product_id=39333`
-    // get request
-    axios.get(url, {
-      headers: {
-        Authorization: TOKEN
-      }
-    })
-    .then(r => setCurrentReviews(r.data.results))
-  }
 
   return (
     <div className="reviews-container">
       <div className="reviews-header">
         RATINGS & REVIEWS
       </div>
-      <div>
-        <ReviewsColumn reviews={reviews} />
-      </div>
-      <div>
-        <div className="sort=by=bar">
-          Sorted by:
-          <select onChange={handleOnChange}>
-            <option value="relevance">relevance</option>
-            <option value="helpful">helpful</option>
-            <option value="newest">newest</option>
-          </select>
-        </div>
-        <ReviewsList reviews={currentReviews}/>
-      </div>
-      <div className="reviews-buttons">
-        {reviews.length > 2 ? <button className="more-reviews-button">MORE REVIEWS</button> : null}
-        <button className="add-review-button">ADD A REVIEW</button>
-      </div>
+      <ReviewsColumn reviews={reviews} />
+      <ReviewsContainer reviews={reviews} />
     </div>
   )
 }
