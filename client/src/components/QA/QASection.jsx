82affa36a7bbca013185cf1893fa2b30d93e9b77
Arun Bhardwaj <arun.b.95@gmail.com>
2021-11-02 21:31:01 -0400

    you can now search through questions and answers

--- client/src/components/QA/QASection.jsx
+++ client/src/components/QA/QASection.jsx
@@ -2,22 +2,51 @@ import React, { useState, useEffect, useReducer, useContext } from 'react';
 import Search from './Search.jsx';
 import QuestionsList from './QuestionsList.jsx';
 import { getAllQuestions } from '../../request.js';
-import { Provider } from './QAContext.jsx';
+import { QAContext } from './QAContext.jsx';
 
 const QASection = () => {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]); // keep track of the questions
+  const [searchResults, setSearchResults] = useState([]); // keep track of searchResults
+  const {search} = useContext(QAContext);
 
   useEffect(() => {
     getAllQuestions(39333)
       .then(results => {
-        console.log(
-          '🚀 ~ file: QASection.jsx ~ line 12 ~ useEffect ~ results.data',
-          results.data.results
-        );
         setQuestions(results.data.results); // Already sorted by Question Helpfulness
       })
-      .catch(err => console.error(err));
+      .catch(err => console.error('There was an error retrieving questions for product >>>', err));
   }, []);
+
+  useEffect(() => {
+    if (search.length >= 3) {
+      getSearchResults();
+    } else {
+      clearSearchResults(); // Clear the searchResults if we don't want to search
+    }
+  }, [search])
+
+  const getSearchResults = () => {
+    let results = [];
+    for (let q of questions) {
+      if (q.question_body.indexOf(search) > -1) {
+        results.push(q);
+        continue;
+      }
+
+      for (let ansKey in q.answers) {
+        if (q.answers[ansKey].body.indexOf(search) > -1) {
+          results.push(q);
+          break;
+        }
+      }
+    }
+
+    setSearchResults(results);
+  }
+
+  const clearSearchResults = () => {
+    setSearchResults([]);
+  }
 
   return (
     <>
@@ -25,7 +54,7 @@ const QASection = () => {
       {/* Putting in a span tag so that childrens listed on the context is just the span as opposed to Question, Answers, &*/}
       <span>QUESTIONS {'&'} ANSWERS</span>
       <Search />
-      <QuestionsList questions={questions} />
+      <QuestionsList questions={questions} searchResults={searchResults}/>
     </>
   );
 };
--- client/src/components/QA/QuestionsList.jsx
+++ client/src/components/QA/QuestionsList.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import QAItem from './QAItem.jsx';
 
-const QuestionsList = ({ questions }) => {
+const QuestionsList = ({ questions, searchResults }) => {
   const [loadFactor, setLoadFactor] = useState(4);
-  let moreQuestions = questions.length - loadFactor;
+  let responses = (searchResults.length) ? searchResults : questions;
+  let moreResponses = responses.length - loadFactor;
+  let renderResults = (searchResults.length) || 0;
 
   return (
     <div>
-      {questions.map((q, idx) =>
+      {responses.map((q, idx) =>
         idx < loadFactor ? <QAItem key={q.question_id} id={q.question_id} question={q} /> : null
       )}
-      {moreQuestions > 0 &&  <button onClick={() => setLoadFactor(loadFactor + 2)}>
-            Load more answered questions ({moreQuestions})
+      {moreResponses > 0 &&  <button onClick={() => setLoadFactor(loadFactor + 2)}>
+            Load more answered questions ({moreResponses})
           </button>}
       {loadFactor > 4 && <button onClick={() => setLoadFactor(4)}>Collapse all questions</button>}
     </div>
