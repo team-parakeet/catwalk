1312bb070d7aad457514979649c246f0e10c12c7
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-03 23:05:12 -0400

    refactored to only contain list logic

--- client/src/components/reviews/ReviewsList.jsx
+++ client/src/components/reviews/ReviewsList.jsx
@@ -2,42 +2,18 @@ import React, { useEffect, useState } from 'react';
 import Review from './Review.jsx';
 
 function ReviewsList({ reviews }) {
-  // what props does this component need?
-  // what state does this component need?
-  const [numReviewsShown, setNumReviewsShown] = useState(2);
-
-  useEffect(() => {
-    setNumReviewsShown(2)
-  }, [reviews])
-
-  const handleMoreReviewsOnClick = () => {
-    numReviewsShown + 2 <= reviews.length ? setNumReviewsShown(prev => prev + 2) : setNumReviewsShown(reviews.length)
-  }
-
-  const handleShowLessOnClick = () => {
-    setNumReviewsShown(2)
-  }
-
-  if (reviews.length === 0) {
+  if (!reviews) {
     return null;
   }
-
-  const reviewsToShow = reviews.slice(0, numReviewsShown);
 
   return (
     <div className="reviews-list-container">
       REVIEWS:
-      {reviewsToShow.map(review => {
+      {reviews.map(review => {
         return <Review key={review.review_id} review={review}/>
       })}
-      <div className="reviews-buttons">
-        {reviews.length < 2 || reviewsToShow.length === reviews.length ? null : <button className="more-reviews-button" onClick={handleMoreReviewsOnClick}>MORE REVIEWS</button> }
-        {reviewsToShow.length === reviews.length ? <button className="show-less-button" onClick={handleShowLessOnClick}>SHOW LESS</button> : null}
-        <button className="add-review-button">ADD A REVIEW</button>
-      </div>
     </div>
   )
 }
-
 
 export default ReviewsList;
