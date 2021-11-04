0363844a0e8e4e738103f7e9cb14400f149f199c
Arun Bhardwaj <arun.b.95@gmail.com>
2021-11-03 21:59:00 -0400

    adds extra api calls

--- client/src/request.js
+++ client/src/request.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import {TOKEN} from '../../config.js';
 
+const header = {
+  Authorization: TOKEN
+};
+
 /**
  * Returns a promise that resolves to all questions for a given product.
  *
@@ -9,11 +13,28 @@ import {TOKEN} from '../../config.js';
  */
 export function getAllQuestions(productId) {
   return axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/qa/questions', {
-    headers: {
-      Authorization: TOKEN
-    },
+    headers: header,
     params: {
       product_id: productId
     }
   })
 }
+
+export function getAllProducts() {
+  return axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products', {
+    headers: header,
+  })
+}
+
+export function getProductInfo(productId) {
+  return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productId}`, {
+    headers: header,
+  })
+}
+
+export function getProductStyles(productId) {
+  return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/products/${productId}/styles`, {
+    headers: header,
+  })
+}
+
