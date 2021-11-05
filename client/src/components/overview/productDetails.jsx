cce9c91c6af7784dac5e15c57b0be3679b4e9581
Kelly <kellytso04@gmail.com>
2021-11-04 21:56:23 -0400

    Created component to house most important product details

---
+++ client/src/components/overview/productDetails.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const ProductDetails = ( {product} ) => {
+  // console.log(product);
+  return (
+      <div className='product-details'>
+        <span>Star rating here</span>
+        <h4>{product.category}</h4>
+        <h2>{product.name}</h2>
+        {product.default_price}
+      </div>
+  )
+}
+
+export default ProductDetails;
