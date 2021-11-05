2b67fdda167311579bc24a4ab839c7b8c5ff3dea
Kelly <kellytso04@gmail.com>
2021-11-04 21:56:03 -0400

    Created component to house product slogan and description

---
+++ client/src/components/overview/productDescription.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const Slogan = styled.div`
+  color: 'hsla(205, 37%, 60%, 50%)';
+`
+
+const ProductDescription = ( {product} ) => {
+  console.log(product);
+
+  return (
+    <div className='product-description'>
+      <Slogan className='slogan' ><h3>{product.slogan}</h3></Slogan>
+      <div className='description description-text' style={ {color: 'hsla(196, 17%, 45%, 1)'} }>{product.description}</div>
+    </div>
+  )
+}
+
+export default ProductDescription;
