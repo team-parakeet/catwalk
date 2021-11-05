c7114e4b71a5ae60a041c7dffeb1d9a99f0f8933
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-04 23:32:52 -0400

    added recommended percentage logic

--- client/src/components/reviews/ReviewsColumn.jsx
+++ client/src/components/reviews/ReviewsColumn.jsx
@@ -4,10 +4,8 @@ import StarsChart from "./StarsChart.jsx"
 import ComparisonScales from "./ComparisonScales.jsx";
 
 function ReviewsColumn({ reviews }) {
-  // what props does this component need?
-  // what state does this component need?
-
   const [ratings, setRatings] = useState({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
+  const [recommendPercent, setRecommendPercent] = useState(null);
 
   useEffect(() => {
     reviews.forEach(review => {
@@ -17,12 +15,27 @@ function ReviewsColumn({ reviews }) {
         [currentRating]: prevState[currentRating] + 1
       }))
     })
+
+    const getRecommendPercentage = () => {
+      let sum = 0;
+      for (let i = 0; i < reviews.length; i++) {
+        let currentReview = reviews[i];
+        if (currentReview.recommend) {
+          sum += 1;
+        }
+      }
+      return Math.round((sum / reviews.length) * 100);
+    }
+    setRecommendPercent(getRecommendPercentage())
   }, [reviews])
 
   return (
     <div className="reviews-column-container">
       <RatingScore reviews={reviews}/>
       <StarsChart ratings={ratings}/>
+      <div className="recommend-percent-bar">
+        {recommendPercent}% of reviews recommend this product
+      </div>
       <ComparisonScales/>
     </div>
   )
