2562491fb03b50076996c619395a29eeacb20f93
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-05 15:16:19 -0400

    readOnly property

--- client/src/components/reviews/Slider.jsx
+++ client/src/components/reviews/Slider.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 function Slider({ id, value }) {
 
   return (
-    <input
+      <input
+      className="slider"
       id={id}
       type="range"
       min="0"
       max="5"
       value={value}
+      readOnly={true}
     />
   );
-
 }
 
  export default Slider;
