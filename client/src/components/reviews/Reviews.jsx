a8f554bd5490bde98b613eba582332b5d1ef2000
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-05 15:13:25 -0400

    passed down productId prop

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReviewsColumn from './ReviewsColumn.jsx';
 import ReviewsContainer from './ReviewsContainer.jsx';
 
-function Reviews({ reviews }) {
+function Reviews({ reviews, productId }) {
   return (
     <div className="reviews-container">
       <div className="reviews-header">
         RATINGS & REVIEWS
       </div>
-      <ReviewsColumn reviews={reviews} />
-      <ReviewsContainer reviews={reviews} />
+      <ReviewsColumn reviews={reviews} productId={productId} />
+      <ReviewsContainer reviews={reviews} productId={productId} />
     </div>
   )
 }
