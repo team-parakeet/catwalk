fca22f19b5da976ff9596e512f375530f6174b17
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-07 17:16:38 -0500

    moved state up to modal component

--- client/src/components/reviews/SelectStars.jsx
+++ client/src/components/reviews/SelectStars.jsx
@@ -1,19 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import StarRatings from 'react-star-ratings';
 
-function SelectStars() {
-
-  const [stars, setStars] = useState(0)
-
-  const handleChangeOnClick = e => {
-    setStars(e)
-  }
-
+function SelectStars({ handleStarChangeOnClick, overallRating }) {
   return (
     <div>
       <StarRatings
-        rating={stars}
-        changeRating={handleChangeOnClick}
+        rating={overallRating}
+        changeRating={handleStarChangeOnClick}
         starDimension="15px"
         starSpacing="0px"
         starHoverColor='rgb(230, 67, 47)'
