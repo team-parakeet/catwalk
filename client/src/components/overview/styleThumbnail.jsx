1de8e675182000e47f432035c2b760e19f3c0924
Kelly <kellytso04@gmail.com>
2021-11-08 15:43:30 -0500

    Refactors StyleThumbnail to be a fn component that uses hooks

--- client/src/components/overview/styleThumbnail.jsx
+++ client/src/components/overview/styleThumbnail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
@@ -13,36 +13,22 @@ const Image = styled.img`
   margin-right: 5px;
 `
 
-class StyleThumbnail extends React.Component {
-  constructor(props) {
-    super(props);
-    // url={url} info={style} selected={t/f} onClick={this.handleStyleSelect}
+const StyleThumbnail = (props) => {
+  // props: url={url} info={style} selected={t/f} onClick={changes styleSelector's state}
 
-    this.state = {
-      isSelected: this.props.selected
-    }
+  const [isSelected, setIsSelected] = useState(props.selected);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    this.setState({
-      isSelected: true,
-    });
-
-    this.props.onClick(this.props.info);
-  }
-
-  render() {
-    return (
-      <Image
-        isSelected={this.props.selected}
-        onClick={this.handleClick}
-        src={this.props.url}
-        alt={this.state.isSelected ? this.props.info.name : null}
-      />
-    )
-  }
+  return (
+    <Image
+      isSelected={props.selected}
+      onClick={ () => {
+        setIsSelected(true);
+        props.onClick(props.info);
+      }}
+      src={props.url}
+      alt={isSelected ? props.info.name : null}
+    />
+  )
 }
 
 export default StyleThumbnail;
