8d1fe7fb1ab288ce2d49d66824327d03a746d0e5
Kelly <kellytso04@gmail.com>
2021-11-09 14:43:55 -0500

    Imports and uses styled Product Detail components

--- client/src/components/overview/productDetails.jsx
+++ client/src/components/overview/productDetails.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import Stars from '../shared/Stars.jsx';
-import styled from 'styled-components';
+import { ProductName, ProductCategory, ProductPrice } from '../styles/Overview/ProductDetails.styled.js';
+// const ProductName = styled.span`
+//   font-size: 30px;
+//   font-style: bold;
+//   color: hsla(196, 17%, 29%, 1);
+// `
 
-const ProductName = styled.span`
-  font-size: 30px;
-  font-style: bold;
-  color: hsla(196, 17%, 29%, 1);
-`
+// const ProductCategory = styled.span`
+//   font-size: 15px;
+//   font-style: italic;
+//   color: hsla(196, 17%, 45%, 85%);
+//   margin-left: 20px;
+// `
 
-const ProductCategory = styled.span`
-  font-size: 15px;
-  font-style: italic;
-  color: hsla(196, 17%, 45%, 85%);
-  margin-left: 20px;
-`
-
-const ProductPrice = styled.div`
-  color: hsla(33, 22%, 45%, 1);
-`
+// const ProductPrice = styled.div`
+//   color: hsla(33, 22%, 45%, 1);
+// `
 
 const ProductDetails = ( {product, rating} ) => {
   return (
