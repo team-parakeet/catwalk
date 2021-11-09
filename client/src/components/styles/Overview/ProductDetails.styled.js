a74d3d380304a2dfbefeb99f94849787b27caa5d
Kelly <kellytso04@gmail.com>
2021-11-09 14:43:24 -0500

    Created styled components to be imported and used in Product Details

---
+++ client/src/components/styles/Overview/ProductDetails.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import index from '../../../index.jsx';
+
+export const ProductName = styled.span`
+  font-size: 30px;
+  font-style: bold;
+  color: hsla(196, 17%, 29%, 1);
+`
+
+export const ProductCategory = styled.span`
+  font-size: 15px;
+  font-style: italic;
+  color: hsla(196, 17%, 45%, 85%);
+  margin-left: 20px;
+`
+
+export const ProductPrice = styled.div`
+  color: hsla(33, 22%, 45%, 1);
+`
