1459e9e9fee3dfd0a05ce0ea4745061d02487c5e
Kelly <kellytso04@gmail.com>
2021-11-09 17:36:26 -0500

    Creates new component ExpandedView to be rendered conditionally in DefaultView

---
+++ client/src/components/overview/ExpandedView.jsx
@@ -0,0 +1,10 @@
+import React, { useContext, useState, useEffect, useCallback } from 'react';
+import { OverviewContext } from './OverviewContext.jsx';
+import { DefaultViewContainer, BackgroundImageContainer, BackgroundImage, ThumbnailSliderContainer } from '../styles/Overview/DefaultView.styled';
+import { Arrow, FadedArrow, ActiveThumbnail, FadedThumbnail, ThumbnailContainer, SliderContainer } from '../styles/Overview/Slider.styled';
+
+const ExpandedView = (props) => {
+
+}
+
+export default ExpandedView;
