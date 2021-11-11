9983cccbc4ae2d9a2bcdffcdda8d16d17d440973
Kelly <kellytso04@gmail.com>
2021-11-11 18:04:01 -0500

    Adds styling to Figure and Image elements

--- client/src/components/styles/Overview/ExpandedView.styled.js
+++ client/src/components/styles/Overview/ExpandedView.styled.js
@@ -1,2 +1,16 @@
 import styled from 'styled-components';
 
+export const Figure = styled.figure`
+  display: flex;
+  width: 600px;
+  height: 600px;
+  background-repeat: no-repeat;
+  justify-content: center;
+`
+
+export const Image = styled.img`
+  display: block;
+  max-width: 500px;
+  max-height: 500px;
+  pointer-events: none;
+`
