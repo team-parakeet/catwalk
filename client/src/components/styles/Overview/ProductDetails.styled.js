ab2e56e1882a8cbbbe42989c6f63db02176db437
Kelly <kellytso04@gmail.com>
2021-11-11 18:02:46 -0500

    Formats the ProductDetailsContainer div

--- client/src/components/styles/Overview/ProductDetails.styled.js
+++ client/src/components/styles/Overview/ProductDetails.styled.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 import index from '../../../index.jsx';
 
-export const ProductName = styled.span`
+export const ProductName = styled.div`
   font-size: 30px;
   font-style: bold;
   color: hsla(196, 17%, 29%, 1);
 `
 
-export const ProductCategory = styled.span`
-  font-size: 15px;
+export const ProductCategory = styled.div`
+  font-size: 20px;
   font-style: italic;
   color: hsla(196, 17%, 45%, 85%);
-  margin-left: 20px;
 `
 
 export const ProductPrice = styled.div`
@@ -19,5 +18,7 @@ export const ProductPrice = styled.div`
 `
 
 export const ProductDetailsContainer = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  margin-left: 30px;
 `
