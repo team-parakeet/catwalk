c7f707c47063316cc6439737d6e862973745ae92
Kelly <kellytso04@gmail.com>
2021-11-11 18:04:22 -0500

    Opens ExpandedView of current image

--- client/src/components/overview/ExpandedView.jsx
+++ client/src/components/overview/ExpandedView.jsx
@@ -1,14 +1,34 @@
 import React, { useContext, useState, useEffect, useCallback } from 'react';
+import styled from 'styled-components';
+import { Figure, Image } from '../styles/Overview/ExpandedView.styled.js';
 import { OverviewContext } from './OverviewContext.jsx';
 import { DefaultViewContainer, BackgroundImageContainer, BackgroundImage, ThumbnailSliderContainer } from '../styles/Overview/DefaultView.styled';
 import { Arrow, FadedArrow, ActiveThumbnail, FadedThumbnail, ThumbnailContainer, SliderContainer } from '../styles/Overview/Slider.styled';
 
-const ExpandedView = (props) => {
-  // TODO: What parts of ExpandedView changes?
+const ExpandedView = ( {src} ) => {
+  // props: src url,
+
   const [ zoom, setZoom ] = useState(false);
+  const [ position, setPosition ] = useState('0% 0%');
+
+  // Handles modal window overflow
+  useEffect(() => {
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = 'hidden';
+    return () => (document.body.style.overflow = originalOverflow);
+  }, []);
+
+  const handleMouseMove = (e) => {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const x = (e.pageX - left) / width * 100;
+    const y = (e.pageY - top) / height * 100;
+    setPosition(`${x}% ${y}%`);
+  }
 
   return (
-    <div>Expanded view btw</div>
+    <Figure class='expanded-view' onMouseMove={handleMouseMove} >
+      <Image class='expanded-thumbnail' src={src} />
+    </Figure>
   )
 }
 
