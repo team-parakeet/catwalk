54e8495d9801a67998cd8e2d00c12566728f4c0e
Kelly <kellytso04@gmail.com>
2021-11-12 10:21:29 -0500

    Lines up RATINGS & REVIEWS with the rest of the component

--- client/src/components/styles/reviews/ReviewsStyled.styled.js
+++ client/src/components/styles/reviews/ReviewsStyled.styled.js
@@ -4,6 +4,7 @@ export const ReviewsHeader = styled.div`
   font-size: 1.5rem;
   background-color: #fff;
   color: #939393;
+  margin-left: 25px;
 `
 export const SectionWrapper = styled.div`
   display: flex;
