c59d36032000ec95f765e3d7d538498d7ed91f8f
Kelly <kellytso04@gmail.com>
2021-11-11 20:53:22 -0500

    Adjusts styling

--- client/src/components/styles/Overview/DefaultView.styled.js
+++ client/src/components/styles/Overview/DefaultView.styled.js
@@ -3,36 +3,23 @@ import styled from 'styled-components';
 export const DefaultViewContainer = styled.div`
   position: relative;
   display: flex;
-  /* max-width: 100vh; */
-  background-color: #EBEBEB;
+  width: auto;
+  max-height: 600px;
   align-items: flex-start;
-  /* max-height: 80vh; */
-  /* outline: 2px solid green; */
+  margin-top: 20px;
   `
 
 export const ThumbnailSliderContainer = styled.div`
-  /* position: absolute;
-  left: 2%;
-  top: 9%; */
   align-self: center;
-  height: auto;
-  /* outline: 2px solid orange; */
-
+  max-height: 600px;
 `
 
-/*
-flexbox
-put backgroudn image in the
-*/
 export const BackgroundImageContainer = styled.div`
   display: flex; // gets rid of some whitespace below the image
-  /* outline: 2px solid blue; */
 `
 
 export const BackgroundImage = styled.img`
-  /* min-height: 100%; */
   max-width: 100%;
-  max-height: 1000px;
-  height: auto;
+  max-height: 600px;
   cursor: url('./images/search.svg'), zoom-in;
 `
--- client/src/components/styles/Overview/ExpandedView.styled.js
+++ client/src/components/styles/Overview/ExpandedView.styled.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 export const Figure = styled.figure`
   display: flex;
-  width: 600px;
-  height: 600px;
+  width: 850px;
+  height: 650px;
   background-repeat: no-repeat;
   justify-content: center;
 `
 
 export const Image = styled.img`
   display: block;
-  max-width: 500px;
-  max-height: 500px;
+  max-width: 450px;
+  max-height: 450px;
   pointer-events: none;
 `
